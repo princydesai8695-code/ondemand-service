cac5c9e9c12ec7cc19d0dac07f05e5e8e21de376
Princy <yourgmail@gmail.com>
2026-05-22 11:48:02 +0530

    Fix all Vercel ESLint errors

--- backend/index.js
+++ backend/index.js
@@ -60,7 +60,7 @@ app.use(cors({
     "http://localhost:3001",
     "http://localhost:5173",
     "http://localhost:5174",
-    "https://your-frontend.onrender.com", // ← replace with your actual frontend URL
+    "https://ondemand-service-jfda.vercel.app", 
   ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
